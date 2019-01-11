78ad81ac03749255de9318d4637b88786b6afc7a
LEE <18381317534@163.com>
2019-01-11 17:36:42 +0800

    Update

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -5,8 +5,64 @@
       <section class="cart-content">
         <div class="store-name">
           <i class="check-all-icon name-common selected"></i>
-          <i class="store-icon name-common"></i>
-          <span class="name-text name-common">小菜在线-自营</span>
+          <div class="name-container">
+            <i class="store-icon name-common"></i>
+            <span class="name-text name-common">小菜在线</span>
+          </div>
+        </div>
+        <div class="cart-goods-list">
+          <div class="cart-goods-item">
+            <div class="check-part">
+              <i class="check-icon selected"></i>
+            </div>
+            <div class="goods-part">
+              <div class="goods-img-box">
+                <img class="goods-img" src="/static/index/5.png" alt="商品图片">
+              </div>
+              <div class="goods-detail-box">
+                <div class="goods-title-box">冰鲜三文鱼冰鲜三文鱼</div>
+                <div class="goods-param-box">电风扇房价快速</div>
+                <div class="goods-price-buy-box">
+                  <p class="price"><span class="currency">￥</span><span class="price-num">58.98</span><span class="unit">/斤</span></p>
+                  <div class="buy-btn-box">
+                    <span class="reduce-btn btn-common"></span>
+                    <input class="input-content" type="text" value="1">
+                    <span class="add-btn btn-common"></span>
+                  </div>
+                </div>
+                <div class="goods-option">
+                  <span class="common-buy common-btn">移入常购</span>
+                  <span class="delete-btn common-btn">删除</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="cart-goods-item">
+            <div class="check-part">
+              <i class="check-icon selected"></i>
+            </div>
+            <div class="goods-part">
+              <div class="goods-img-box">
+                <img class="goods-img" src="/static/index/9.png" alt="商品图片">
+              </div>
+              <div class="goods-detail-box">
+                <div class="goods-title-box">超级美味八爪鱼</div>
+                <div class="goods-param-box">电风扇房价快速</div>
+                <div class="goods-price-buy-box">
+                  <p class="price"><span class="currency">￥</span><span class="price-num">58.98</span><span class="unit">/斤</span></p>
+                  <div class="buy-btn-box">
+                    <span class="reduce-btn btn-common"></span>
+                    <input class="input-content" type="text" value="1">
+                    <span class="add-btn btn-common"></span>
+                  </div>
+                </div>
+                <div class="goods-option">
+                  <span class="common-buy common-btn">移入常购</span>
+                  <span class="delete-btn common-btn">删除</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
@@ -51,33 +107,182 @@ export default {
       border-image: svg(1px-border param(--color #eee)) 1 stretch;
     }
     .cart-content{
-      margin: 20px 32px 0 32px;
-      .check-all-icon{
-        width: 36px;
-        height: 36px;
-        background-image: url("../assets/img/shop/check-no.png");
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: 36px 36px;
-        margin-right: 25px;
+      margin: 25px 32px 0 32px;
+      .store-name{
+        .check-all-icon{
+          display: inline-block;
+          vertical-align: middle;
+          width: 36px;
+          height: 36px;
+          background-image: url("../assets/img/shop/check-no.png");
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: 36px 36px;
+          margin-right: 25px;
+        }
+        .check-all-icon.selected{
+          background-image: url("../assets/img/shop/check-ok.png");
+        }
+        .name-container{
+          display: inline-block;
+          .store-icon{
+            display: inline-block;
+            vertical-align: middle;
+            width: 32px;
+            height: 32px;
+            background-image: url("../assets/img/shop/store-icon.png");
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: 32px 32px;
+            margin-right: 10px;
+          }
+          .name-text{
+            display: inline-block;
+            // height: 32px;
+            vertical-align: middle;
+            // line-height: 32px;
+            font-size: 32px;
+          }
+        }
       }
-      .check-all-icon.selected{
-        background-image: url("../assets/img/shop/check-ok.png");
-      }
-      .store-icon{
-        width: 32px;
-        height: 32px;
-        background-image: url("../assets/img/shop/store-icon.png");
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: 32px 32px;
-        margin-right: 10px;
-      }
-      .name-common{
-        display: inline-block;
-        vertical-align: middle;
-        font-size: 30px;
-      }
+      .cart-goods-list{
+        margin-top: 40px;
+        .cart-goods-item{
+          font-size: 0;
+          margin-bottom: 25px;
+          .check-part{
+            display: inline-block;
+            vertical-align: middle;
+            width: 10%;
+            .check-icon{
+              display: inline-block;
+              width: 36px;
+              height: 36px;
+              background-image: url("../assets/img/shop/check-no.png");
+              background-repeat: no-repeat;
+              background-position: center;
+              background-size: 36px 36px;
+            }
+            .check-icon.selected{
+              background-image: url("../assets/img/shop/check-ok.png");
+            }
+          }
+          .goods-part{
+            display: inline-block;
+            vertical-align: middle;
+            width: 90%;
+            border-bottom: 1px solid transparent;
+            border-image: svg(1px-border param(--color #e5e5e5)) 1 stretch;
+            .goods-img-box{
+              display: inline-block;
+              vertical-align: top;
+              width: 130px;
+              height: 130px;
+              .goods-img{
+                width: 130px;
+                height: 130px;
+              }
+            }
+            .goods-detail-box{
+              display: inline-block;
+              min-height: 120px;
+              width: 78.5%;
+              box-sizing: border-box;
+              padding-left: 0.3rem;
+              .goods-title-box{
+                font-size: 28px;
+                height: 75px;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 2;
+                -webkit-box-orient: vertical;
+                margin-bottom: 15px;
+              }
+              .goods-param-box{
+                height: 40px;
+                line-height: 40px;
+                color: #666;
+                border: 1px solid #e5e5e5;
+                box-sizing: border-box;
+                padding-left: 10px;
+                padding-right: 30px;
+                border-radius: 5px;
+                margin-bottom: 15px;
+                background-image: url("../assets/img/shop/down.png");
+                background-repeat: no-repeat;
+                background-position: right center;
+                background-size: 36px 36px; 
+              }
+              .goods-price-buy-box{
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                .price{
+                  .currency{
+                    color: #FF1F1F;
+                  }
+                  .price-num{
+                    color: #FF1F1F;
+                    font-size: 30px;
+                  }
+                  .unit{
+                    color: #999;
+                  }
+                }
+                .buy-btn-box{
+                  font-size: 0;
+                  .btn-common{
+                    display: inline-block;
+                    vertical-align: middle;
+                    width: 50px;
+                    height: 50px;
+                    background-color: #f7f7f7;
+                  }
+                  .input-content{
+                    vertical-align: middle;
+                    text-align: center;
+                    background-color: #f7f7f7;
+                    color: #999;
+                    -webkit-appearance: none;
+                    border: 0;
+                    height: 50px;
+                    width: 80px;
+                    line-height: 50px;
+                  }
+                  .reduce-btn{
+                    margin-right: 10px;
+                    background-image: url("../assets/img/shop/reduce.png");
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    background-size: 36px 36px;
+                  }
+                  .reduce-btn.reduce-zero{
+                    opacity: 0.25;
+                    pointer-events: none;
+                  }
+                  .add-btn{
+                    margin-left: 10px;
+                    background-image: url("../assets/img/shop/add.png");
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    background-size: 36px 36px;
+                  }
+                }
+              }
+              .goods-option{
+                text-align: right;
+                color: #999;
+                margin-top: 10px;
+                margin-bottom: 10px;
+                .common-btn{
+                  margin-left: 20px;
+                }
+              }
+            }
+          }
+        }
+      } 
     }
   }
 </style>
