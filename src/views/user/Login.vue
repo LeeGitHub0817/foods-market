0a974ac8993c2cadd1238f1a050e62772b4e7e8d
18381317534@163.com <18381317534@163.com>
2019-04-14 17:05:58 +0800

    Update 04141705

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -36,4 +36,4 @@ export default {
 }
 </script>
 
-<style lang="scss" src="../assets/sass/user.scss"></style>
+<style lang="scss" src="@/assets/sass/user.scss"></style>
--- src/views/user/Retrieve.vue
+++ src/views/user/Retrieve.vue
@@ -9,12 +9,7 @@
       <div class="l-common l-code">
         <div class="common code-con">
           <input type="text" placeholder="验证码">
-          <div @click="getCheckCode($event)" class="getcode-btn getcode">获取验证码</div>
-        </div>
-      </div>
-      <div class="l-common l-invitecode">
-        <div class="common invitecode-con">
-          <input type="text" placeholder="邀请码">
+          <div @click="getCheckCode($event)" :class="{'getcode-btn': true, getcode: !isShowTimeCount, countdown: isShowTimeCount}">获取验证码</div>
         </div>
       </div>
       <div class="l-common l-upwd">
@@ -22,24 +17,16 @@
           <input type="password" placeholder="密码">
         </div>
       </div>
-      <div class="l-common l-protocol">
-        <div class="common protocol-con">
-          <input style="position:absolute; left: 0; top: 0; visibility: hidden;" type="checkbox" value="注册" :checked="isAgreePoliy">
-          <em @click="handleSelectPolicy()" :class="{'check-btn': true, selected: isAgreePoliy}"></em>
-          <span class="part-one">我已阅读并同意</span>
-          <span class="part-two"><a href="">《注册协议》</a></span>
-        </div>
-      </div>
       <div class="l-common l-loginbtn">
-        <div :class="{'common loginbtn-con regbtn-con': true, active: isAgreePoliy}">
-          <input style="-webkit-appearance: none;" type="button" value="注册">
+        <div class="common loginbtn-con">
+          <input style="-webkit-appearance: none;" type="button" value="确定并登录">
         </div>
       </div>
       <div class="l-common l-link">
         <div class="link-con">
           <span class="l-reg"><router-link to="/login">账号登录</router-link></span>
           <span class="l-line"></span>
-          <span class="l-ret"><router-link to="/retrieve">找回密码</router-link></span>
+          <span class="l-ret"><router-link to="/register">注册账户</router-link></span>
         </div>
       </div>
     </form>
@@ -50,12 +37,11 @@
 export default {
   data: function(){
     return {
-      isShowTimeCount: false,
-      isAgreePoliy: false
+      isShowTimeCount: false //用来判断是否点击了获取验证码
     }
   },
   methods: {
-    getCheckCode: function(e){ //获取验证码
+    getCheckCode: function(e){
       var curTarget = e.target;
       var time = 60;
       // curTarget.classList.add("countdown");
@@ -71,12 +57,9 @@ export default {
           this.isShowTimeCount = false;
         }
       }, 1000);
-    },
-    handleSelectPolicy: function(){ //是否同意协议
-      this.isAgreePoliy = this.isAgreePoliy === false ? true : false;
     }
   }
 }
 </script>
 
-<style lang="scss" src="../assets/sass/user.scss"></style>
+<style lang="scss" src="@/assets/sass/user.scss"></style>
